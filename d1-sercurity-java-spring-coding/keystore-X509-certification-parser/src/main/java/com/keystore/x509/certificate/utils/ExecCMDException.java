ea28282755f85fc3622b321f2e5d33f225173135
johnzhang320 <johnzhang320@gmail.com>
2019-07-01 10:15:27 -0700

    John Zhang Code 07-01-2019

--- d1-sercurity-java-spring-coding/keystore-X509-certification-parser/src/main/java/com/keystore/x509/certificate/utils/ExecCMDException.java
+++
@@ -1,8 +0,0 @@
-package com.keystore.x509.certificate.utils;
-public class ExecCMDException extends RuntimeException {
-
-    public ExecCMDException(final String message) {
-        super(message);
-    }
-
-}
